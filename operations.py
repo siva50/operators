6d2722adf5e398b726a2ce076c1c0e2f7cf45343
siva50 <80094666+siva50@users.noreply.github.com>
2022-07-18 20:16:33 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,14 @@
+num1=int(input("enter first number:"))
+num2=int(input("enter secound number:"))
+s=num1+num2
+d=num1-num2
+p=num1*num2
+q=num1/num2
+r=num1%num2
+e=num1**num2
+print(s)
+print(d)
+print(p)
+print(q)
+print(r)
+print(e)
